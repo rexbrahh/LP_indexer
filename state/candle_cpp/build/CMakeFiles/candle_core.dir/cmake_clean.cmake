ef875db27dca9aecca6cfdf1f1dd2aa7b666ea42
Rex Liu <hi@r3x.sh>
2025-10-16 00:14:41 -0400

    initial dump

---
+++ state/candle_cpp/build/CMakeFiles/candle_core.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/candle_core.dir/src/candle_worker.cpp.o"
+  "CMakeFiles/candle_core.dir/src/candle_worker.cpp.o.d"
+  "CMakeFiles/candle_core.dir/src/fixed_point.cpp.o"
+  "CMakeFiles/candle_core.dir/src/fixed_point.cpp.o.d"
+  "libcandle_core.a"
+  "libcandle_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/candle_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ state/candle_cpp/build/CMakeFiles/candle_fixed_point_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/candle_fixed_point_test.dir/tests/fixed_point_test.cpp.o"
+  "CMakeFiles/candle_fixed_point_test.dir/tests/fixed_point_test.cpp.o.d"
+  "candle_fixed_point_test"
+  "candle_fixed_point_test.pdb"
+  "candle_fixed_point_test[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/candle_fixed_point_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
