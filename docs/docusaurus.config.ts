0e689b3612f13d7e5a266e3fd67821770385ffc7
Rex Liu <hi@r3x.sh>
2025-12-14 21:00:20 -0500

    Docs add cross-links and indexes

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -28,6 +28,11 @@ const config: Config = {
   projectName: 'LP_indexer', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
